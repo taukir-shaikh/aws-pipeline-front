78a2222ab9baa6db152bb4bb3418587a0e577d82
taukir-shaikh <160217826+taukir-shaikh@users.noreply.github.com>
2025-10-07 00:47:42 +0530

    added chakra setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App'
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const theme = extendTheme({ colors })
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
